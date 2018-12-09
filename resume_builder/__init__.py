a49d5d0dcfc1188cd11457ad40d29a79ecda7980
Paul Ganssle <paul@ganssle.io>
2018-12-09 14:38:10 -0500

    Add slicing and any/all functions to Jinja

--- resume_builder/__init__.py
+++ resume_builder/__init__.py
@@ -35,8 +35,38 @@ def get_event_details(talk):
     return name, location
 
 
+def slice_values(values, subset):
+    """
+    Overload the slicing concept to allow YAML-storable values to be used
+    as slices.
+
+    If subset is a:
+
+        ``bool``: Return the entire list for ``True``, nothing for ``False``
+        ``int``: Return ``values[0:subset]``
+        ``list`` or ``tuple`` of length 2: Return ``values[subset[0]:subset[1]]``
+    """
+    if isinstance(subset, bool):
+        if subset:
+            return values
+        else:
+            return []
+
+    if isinstance(subset, int):
+        subset = (0, subset)
+
+    if isinstance(subset, (list, tuple)):
+        assert len(subset) == 2
+        return values[subset[0]:subset[1]]
+
+    raise TypeError(f"Cannot process subset of class {type(subset).__name__}")
+
+
 def main():
     builder = resume_builder.ResumeBuilder(
-        custom_funcs={'get_event_details': get_event_details}
+        custom_funcs={'get_event_details': get_event_details,
+                      'slice_values': slice_values,
+                      'any': any,
+                      'all': all}
     )
     builder.write()
